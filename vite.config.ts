a68085515fc8626a4b0e640d08224e6a362d35d6
mauricioroseroperezvs-arch <mauricioroseroperezvs@gmail.com>
2026-01-08 20:20:12 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,12 @@ export default defineConfig({
 
   server: {
     port: 5173,
-    host: true
+    host: true,    // Permite la conexión desde cualquier host
+    allowedHosts: 'all'  // Asegura que Vite acepte peticiones de cualquier host
   },
 
   preview: {
     port: 3000,
-    host: true
+    host: true    // Asegura que esté accesible en cualquier entorno
   }
 });
