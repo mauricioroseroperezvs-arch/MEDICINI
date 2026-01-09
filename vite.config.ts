d56c755480b96425549ded65c4f35dda8b408b6b
mauricioroseroperezvs-arch <mauricioroseroperezvs@gmail.com>
2026-01-08 21:42:05 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/", // <- importante para producción en Vercel
   plugins: [react()],
 
   server: {
